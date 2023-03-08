b636e08163cabe7a30c653e95c0767477e52b7b1
Nikita Kulakov <90207780+NikitaDestroyer@users.noreply.github.com>
2023-03-08 12:26:04 +0300

    Add files via upload

---
+++ Virus.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include <math.h>
+#include <conio.h>
+
+using namespace std;
+int main()
+{
+    int n, m, quant, minim_dist, maxim=0, t;
+    cin >> n >> m;
+    cin >> quant;
+    vector<int> x(n);
+    vector<int> y(m);
+    for (int i = 0; i < quant; ++i) {
+        cin >> x[i] >> y[i];
+    }
+    
+
+    for (int i = 1; i < n + 1; ++i) {
+        for (int j = 1; j < m + 1; ++j)
+        {
+            minim_dist = n + m;
+            for (int u = 0; u < quant; ++u){
+                t = (abs(i - x[u]) + abs(j - y[u]));
+                if (t < minim_dist) {
+                    minim_dist = t;
+                }
+            }
+            if (minim_dist > maxim) {
+                maxim = minim_dist;
+            }
+        }
+    }
+    cout << maxim << endl;
+}
+
+
+
+
+
+
